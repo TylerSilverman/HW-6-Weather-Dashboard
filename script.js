a93eae889f9b80e9ce735d03a8bb51f834cdef5b
Tyler Silverman <silverman.tyler@gmail.com>
2020-10-16 15:04:09 -0400

    creating a function to save search bar history to local storage, also commented in html to remind myself I need a header

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-var searchCity = localStorage.getItem("#");
-$("#searchCity").val(searchCity);
+var searchCity = localStorage.getItem("#searchCity");
 
-localStorage.setItem('#searchCity', searchCity );
-console.log("search of city saved");
+
+
+$("#seachCityBtn").on("click", function (){
+    var search = $("#searchCity").val();
+
+    localStorage.setItem('#searchCity', search);
+    console.log("search of city saved");
+
+    upDateHistory()
+
+})
+
+
+// function has a value to make 
+// and creating an if statement 
+
+// function upDateHistory (){
+//     items from localStorage and then do a loop to append to city history
+// }
