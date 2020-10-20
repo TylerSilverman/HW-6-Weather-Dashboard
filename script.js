73e51885737957d084ee8cd00b215783d9ed75aa
Tyler Silverman <silverman.tyler@gmail.com>
2020-10-19 21:26:33 -0400

    added some comments to help me stay organized

--- script.js
+++ script.js
@@ -16,12 +16,13 @@ $(document).ready(function(){
         e.preventDefault();
         var searchCity = $("#searchCity").val();
 
+//function for the ajax and to retrive the infomation from the open weather map site with the API key
         $.ajax({
             url:"http://api.openweathermap.org/data/2.5/weather?id=524901&appid=f80ecd3a4607dd7ef4888b33f7c6f3e5",
             type: "GET",
             dataType: "JSON",
             data: {q:searchCity, appid:apiKey, units:'metric'},
-            
+            // function to retrieve the data which shows up in the netweork part of the console log
             function (data){
                 var weatherForecast = "";
                 $.each(data.weather, function(data, val){
@@ -54,3 +55,9 @@ $(document).ready(function(){
 
  function resetData (){  
  };
+
+ //Need to create a function to have the information post on the HTML document. 
+    //create and .then statement to retrive the information 
+
+    
+ // Need to create a function to show the information adn also style the information 
