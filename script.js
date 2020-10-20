92f11963651534c81e6ecd399f4770ed0f3eb3e9
Tyler Silverman <silverman.tyler@gmail.com>
2020-10-20 13:18:41 -0400

    cleaned up some javascript and still trying to figure out how to display the information from the network section in the concole.log

--- script.js
+++ script.js
@@ -14,16 +14,16 @@ $(document).ready(function(){
     $("#searchBtn").click(function(e){
         e.preventDefault();
         var searchCity = $("#searchCity").val();
-        var url = buildQueryUrl ()
+        var url = buildQueryUrl ();
 
-//function for the ajax and to retrive the infomation from the open weather map site with the API key
+        //function for the ajax and to retrive the infomation from the open weather map site with the API key
 
-function buildQueryUrl () {
-    var url = "http://api.openweathermap.org/data/2.5/weather?id=524901&appid=f80ecd3a4607dd7ef4888b33f7c6f3e5";
-    var apiKey = {"apiKey": apiKey};
-    apiKey.q = $("#searchCity").val().trim();
+    function buildQueryUrl () {
+        var url = "http://api.openweathermap.org/data/2.5/weather?id=524901&appid=f80ecd3a4607dd7ef4888b33f7c6f3e5";
+        var apiKey = {"apiKey": apiKey};
+        apiKey.q = $("#searchCity").val().trim();
 
-}
+};
 
         $.ajax({
             url:"http://api.openweathermap.org/data/2.5/weather?id=524901&appid=f80ecd3a4607dd7ef4888b33f7c6f3e5",
@@ -35,9 +35,9 @@ function buildQueryUrl () {
         localStorage.setItem('#searchCity',JSON.stringify(myArray));
         console.log(myArray)
         localStorage.getItem('#searchCity');
-        
     });
 });
+
 
 //function to try and append the searches on the html page 
  function addData(){
@@ -50,26 +50,23 @@ function buildQueryUrl () {
 
      for (i=0; i<myArray.length; i++) {
         pval = pval + myArray[i] + "<br/>";
-    }
+    };
     document.getElementById("cityList").innerHTML = pval;
- }  
+    
+ };
 
  //function to clear out the search 
  function clear (){
      $("#weather-section").empty();
- }
+ };
  $("#clearCityBtn").on("click", clear);
+
  
 
  //Need to create a function to have the information post on the HTML document. 
     //create and .then statement to retrive the information 
+function display (){
 
+}
     
  // Need to create a function to show the information adn also style the information 
-//  function (data){
-//     var weatherForecast = "";
-//     $.each(data.weather, function(data, val){
-//         weatherForecast = data.name + val.icon + data.main.temp + " &deg;C " + " | " + val.main + ", " + val.description
-//     });
-//     $("#searchCity").HTML(weatherForecast);
-// };
