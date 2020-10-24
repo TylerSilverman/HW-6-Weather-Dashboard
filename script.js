d4b3d6a9891b843f6df7e6627b0d7ff8ba429ef6
Tyler Silverman <silverman.tyler@gmail.com>
2020-10-24 10:51:28 -0400

    removed the onclick function in HTML and cleaned up the script file

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var apiKey = "f80ecd3a4607dd7ef4888b33f7c6f3e5";
 console.log(apiKey);
 var url = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f80ecd3a4607dd7ef4888b33f7c6f3e5";
 var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=f80ecd3a4607dd7ef4888b33f7c6f3e5";
-// var forecastWeatherUrl =  "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f80ecd3a4607dd7ef4888b33f7c6f3e5";
 
 var createBtn = document.getElementById ("newBtn");
 
@@ -13,20 +12,11 @@ $(document).ready(function(){
     $("#searchBtn").submit(function(event){
         ready(event);
 
-        var searchBtn =$("#searchBtn").val();
-
-        document.getElementById("Btn").innerHTML += "<button>" + myArray[i] + "</button>";
+        var searchBtn =$("#searchBtn").val(createBtn);
+        
 
     });
 });
-//creating an array for buttons
-// function addButton () {
-//     for (var i=0; i < myArray.length; i++) {
-//         document.getElementById("newBtn").innerHTML += "<button>" + myArray[i] + "</button>";
-//     }
-// }
-// addButton ();
-
 
 //setting the current Time
 $(document).ready(function(){
@@ -46,13 +36,13 @@ $(document).ready(function(){
 
         //function for the ajax and to retrive the infomation from the open weather map site with the API key
 
-    function buildQueryUrl () {
+        function buildQueryUrl () {
         var url = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f80ecd3a4607dd7ef4888b33f7c6f3e5";
         var uvUrl = uvUrl;
         var apiKey = {"apiKey": apiKey};
         apiKey.q = $("#searchCity").val().trim();
 
-};
+
         //ajax function for the weather for the current day 
         $.ajax({
             url:"https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f80ecd3a4607dd7ef4888b33f7c6f3e5",
@@ -63,21 +53,22 @@ $(document).ready(function(){
             }).then(function(response){
             console.log(response);
 
-            for (var i=0; i < myArray.length; i+=5){
-
-                // function to have the information post on the HTML document. 
-                var cityName =$("<strong><h3><strong>").text(searchCity);
-                var temp_min =$("<h6>").text("Temperature: " + response.main.temp_min + " *F ");
-                var temp_max =$("<h6>").text("Temperature: " + response.main.temp_max + " *F ");
-                var humidity =$("<h6>").text("Humidity: " + response.main.humidity + " % ");
-                var speed = $("<h6>").text("Wind Speed: " + response.wind.speed + " MPH ");
-                var pressure = $("<h6>").text("Pressure: " + response.main.pressure + " Air ");
-                var coord = $("<h6>").text("Coordinates: Latitude: " + response.coord.lat + "  " + " " + " Longitude: " +  response.coord.lon);
             
 
-            $('.weather-info').empty().append(cityName, temp_max, temp_min, humidity, speed, pressure, coord)
-        }
-            //make another ajax call to get the 5 day forecast, generate the UI, need to change to 5day forecast
+                for (var i=0; i < myArray.length; i+=5){
+
+                    // function to have the information post on the HTML document. 
+                    var cityName =$("<strong><h3><strong>").text(city.coord.name);
+                    var tempMin =$("<h6>").text("Temperature: " + list.main.temp_min + " *F ");
+                    var tempMax =$("<h6>").text("Temperature: " + list.main.temp_max + " *F ");
+                    var humidity =$("<h6>").text("Humidity: " + response.main.humidity + " % ");
+                    var speed = $("<h6>").text("Wind Speed: " + response.wind.speed + " MPH ");
+                    var pressure = $("<h6>").text("Pressure: " + response.main.pressure + " Air ");
+                    var coord = $("<h6>").text("Coordinates: Latitude: " + response.coord.lat + "  " + " " + " Longitude: " +  response.coord.lon);
+                
+
+                    $('.weather-info').empty().append(cityName, tempMin, tempMax, humidity, speed, pressure, coord)
+                }
         $.ajax({
                 url:"https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f80ecd3a4607dd7ef4888b33f7c6f3e5",
                 method: "GET",
@@ -87,64 +78,28 @@ $(document).ready(function(){
                 }).then(function(weatherForecastRes){
                     console.log(weatherForecastRes);
 
-                for (var i=0; i < myArray.length; i+=8){
+                for (var i=0; i < weatherForecastRes.length; i+=8){
 
                     var cityName =$("<strong><h2><strong>").text(searchCity);
-                    var temp_min =$("<h6>").text("Temperature: " + response.main.temp_min + " *F ");
-                    var temp_max =$("<h6>").text("Temperature: " + response.main.temp_max + " *F ");
-                    var humidity =$("<h6>").text("Humidity: " + response.main.humidity + " % ");
-                    var speed = $("<h6>").text("Wind Speed: " + response.wind.speed + " MPH ");
-                    var pressure = $("<h6>").text("Pressure: " + response.main.pressure + " Air ");
-                    var coord = $("<h6>").text("Coordinates: Latitude: " + response.coord.lat + "  " + " " + " Longitude: " +  response.coord.lon);
+                    var temp_min =$("<h6>").text("Temperature: " + weatherForecastRes.main.temp_min + " *F ");
+                    var temp_max =$("<h6>").text("Temperature: " + weatherForecastRes.main.temp_max + " *F ");
+                    var humidity =$("<h6>").text("Humidity: " + weatherForecastRes.main.humidity + " % ");
+                    var speed = $("<h6>").text("Wind Speed: " + weatherForecastRes.wind.speed + " MPH ");
+                    var pressure = $("<h6>").text("Pressure: " + weatherForecastRes.main.pressure + " Air ");
+                    var coord = $("<h6>").text("Coordinates: Latitude: " + weatherForecastRes.coord.lat + "  " + " " + " Longitude: " +  response.coord.lon);
                     }
         
                 $('#weather-section').empty().append(cityName, temp_max, temp_min, humidity, speed, pressure, coord)
             });
         });
         localStorage.setItem('#searchCity',JSON.stringify(myArray));
-        console.log(myArray)
+        console.log(searchCity)
         localStorage.getItem('#searchCity');
-    });
-});
-    //ajax function for the UV index
-    // $.ajax({
-    //     uvUrl:uvUrl,
-    //     method: "GET",
-    //     dataType: "JSON",
-    //     data: {q:searchCity, appid:apiKey, units:'imperial'},
-    //     // function to retrieve the data which shows up in the netweork part of the console log
-    //     }).then(function(uvResponse){
-    //     console.log(uvResponse);
-    // });
-
-
-//function to try and append the searches on the html page 
- function addData(){
-     var inputCityList = document.getElementById 
-     ("searchCity").value;
-
-     myArray.push(inputCityList);
-     //created pval for a list to descend the city names
-     var pval = "";
-
-     for (i=0; i<myArray.length; i++) {
-        pval = pval + myArray[i] + "<br/>";
     };
-    document.getElementById("cityList").innerHTML = pval;
-    
- };
-
- //function to clear out the search 
- function clear (){
-     $("#weather-section").empty();
- };
- $("#clearCityBtn").on("click", clear);
-
- 
-
- //create and .then statement to retrive the information 
-// // create a button for input textbox
-// $('.createBtn').on("click", function() {
-//     var cityList = $("#searchCity").val().trim();
-//     console.log(cityList);
-// });
+});
+    //function to clear out the search 
+    function clear (){
+        $("#weather-section").empty();
+    };
+    $("#clearCityBtn").on("click", clear);
+});
